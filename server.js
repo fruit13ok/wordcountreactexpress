0786efea16779475cee035d99af71ce9b848da63
fruit13ok <fruit13ok@gmail.com>
2019-04-24 16:33:18 -0700

    heroku

--- server.js
+++ server.js
@@ -3,16 +3,7 @@ const request = require('request');
 const cheerio = require('cheerio');
 
 const app = express();
-if (process.env.NODE_ENV === 'production') {
-    // Exprees will serve up production assets
-    app.use(express.static('client/build'));
-  
-    // Express serve up index.html file if it doesn't recognize route
-    const path = require('path');
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
+
 app.get('/api/wordcount', (req, res) => {
     var url = req.query.query || 'https://www.google.com';
     console.log('url: ',url);
@@ -55,6 +46,16 @@ app.get('/api/wordcount', (req, res) => {
     });
 });
 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => `Server running on port ${port}`);
